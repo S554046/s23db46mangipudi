1323b4d5afb62db63f04020223ef7e2830c5ba66
S554046 <S554046@nwmissouri.edu>
2023-04-27 22:22:43 -0500

    Final Commit

--- models/fish.js
+++ models/fish.js
@@ -1,7 +1,15 @@
 const mongoose = require("mongoose")
 const fishSchema = mongoose.Schema({
 fish_type: String,
-fish_name: String,
-fish_cost: Number
+fish_name: {
+    type: String,
+    required: true,
+    match: /^[a-zA-Z]+$/
+},
+fish_cost: {
+    type: Number,
+    min:1,
+    max:10000
+} 
 })
 module.exports = mongoose.model("fish",fishSchema)
--- routes/fish.js
+++ routes/fish.js
@@ -31,9 +31,18 @@ router.get('/detail', fish_controlers.fish_view_one_Page);
 /* GET create fish page */
 router.get('/create', fish_controlers.fish_create_Page);
 
-
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ req.session.returnTo = req.originalUrl;
+ res.redirect("/login");
+ }
 /* GET create update page */
-router.get('/update', fish_controlers.fish_update_Page);
+router.get('/update',secured, fish_controlers.fish_update_Page);
 
 router.get('/delete', fish_controlers.fish_delete_Page);
 
